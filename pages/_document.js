498c81df4aa15dd6646b509663251e32c9e4038d
Sayandip Adhikary <sayandipadhikary@Sayandips-MacBook-Air.local>
2024-12-13 21:53:42 +0530

    updated script

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export const metadata = {
   title: "SayanDip Adhikary",
-  description: 'Welcome to the official portfolio of Sayandip Adhikary, a skilled Software Engineer and Frontend Developer with expertise in the MERN stack. Explore my projects, experience, and skills in web development, UI/UX design, and more. Whether you are looking for innovative solutions or engaging user interfaces, I specialize in creating dynamic and responsive websites and web applications. View my work and get in touch for collaboration opportunities.',
+  description: "Welcome to the official portfolio of Sayandip Adhikary, a skilled Software Engineer and Frontend Developer with expertise in the MERN stack. Explore my projects, experience, and skills in web development, UI/UX design, and more. Whether you are looking for innovative solutions or engaging user interfaces, I specialize in creating dynamic and responsive websites and web applications. View my work and get in touch for collaboration opportunities.",
   siteUrl: "https://sayandip-adhikary.vercel.app/",
   imageUrl: "https://sayandip-adhikary.vercel.app/Me2.jpeg"
 };
@@ -29,29 +29,61 @@ export default function Document() {
 
         <meta name="robots" content="index, follow" />
 
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebPage",
-            "name": "SayanDip Adhikary",
-            "url": metadata.siteUrl,
-            "description": metadata.description,
-            "image": metadata.imageUrl,
-            "mainEntityOfPage": metadata.siteUrl,
-            "publisher": {
-              "@type": "Organization",
+        {/* <script type="application/ld+json">
+          {JSON.stringify(
+            {
+              "@context": "https://schema.org",
+              "@type": "WebPage",
               "name": "SayanDip Adhikary",
-              "logo": metadata.imageUrl
-            },
-            "contactPoint": {
-              "@type": "ContactPoint",
-              "email": "adhikarysayandip@gmail.com",
-              "contactType": "Job or project offers",
-              "areaServed": "IN",
-              "availableLanguage": "English"
-            }
-          }, null, 2)}
-        </script>
+              "url": metadata.siteUrl,
+              "description": metadata.description,
+              "image": metadata.imageUrl,
+              "mainEntityOfPage": metadata.siteUrl,
+              "publisher": {
+                "@type": "Organization",
+                "name": "SayanDip Adhikary",
+                "logo": metadata.imageUrl
+              },
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "email": "adhikarysayandip@gmail.com",
+                "contactType": "Job or project offers",
+                "areaServed": "IN",
+                "availableLanguage": "English"
+              }
+            }, null, 2)}
+        </script> */}
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(
+              {
+                "@context": "https://schema.org",
+                "@type": "WebPage",
+                name: "SayanDip Adhikary",
+                url: metadata.siteUrl,
+                description: metadata.description,
+                image: metadata.imageUrl,
+                mainEntityOfPage: metadata.siteUrl,
+                publisher: {
+                  "@type": "Organization",
+                  name: "SayanDip Adhikary",
+                  logo: metadata.imageUrl,
+                },
+                contactPoint: {
+                  "@type": "ContactPoint",
+                  email: "adhikarysayandip@gmail.com",
+                  contactType: "Job or project offers",
+                  areaServed: "IN",
+                  availableLanguage: "English",
+                },
+              },
+              null,
+              2
+            ),
+          }}
+        />
       </Head>
       <body>
         <Main />
